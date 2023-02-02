3d6c49ca040a6bdd71dc658195a7849f66709da5
tran.huycerebral <tran.huy@cerebral.com>
2023-02-02 18:43:10 +0700

    update workflow

--- README.md
+++ README.md
@@ -4,4 +4,22 @@ Install astro cli in your local machine with the following command
 ```bash
 brew install astro
 ```
+## Run astro on local 
+```bash
+git clone git@github.com:tranhuycerebral/airflow_introduction.git
+cd airflow_introduction
+astro dev start
+```
 ## Implement the sample workflow
+Finish the dag `./dag/example-dag.py` and move to the next step
+![alt text](https://github.com/tranhuycerebral/airflow_introduction/blob/main/sample_workflow.png?raw=true)
+
+## Restart astro and verify the pipeline
+```bash
+astro dev restart
+```
+
+## Cleanup
+```bash
+astro dev stop
+```
--- dags/example-dag.py
+++ dags/example-dag.py
@@ -1,53 +1,57 @@
 """Create DAG to show good practices."""
 
 import datetime
+import time
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
-
 def send_failure_message():
-    """Create dummy test suite to exemplify."""
     print("Dag run fail")
 
 
-def task_1():
-    """Create dummy test suite to exemplify."""
-    print("Processing task 1")
+def download_invoices():
+    print("Dowloading invoice!!!")
+    time.sleep(5)
+    print("Downloaded!")
 
 
-def task_2():
-    """Create dummy test suite to exemplify."""
-    print("Processing task 2")
+def transform_invoices():
+    print("Transform invoice")
 
 
-def task_3_1():
-    """Create dummy test suite to exemplify."""
-    print("Processing task 3.1")
+def save_to_database():
+    print("Save invoices to database")
 
 
-def task_3_2():
-    """Create dummy test suite to exemplify."""
-    print("Processing task 3.2")
+def generate_report():
+    print("Generate report")
+
+
+def notify_via_email():
+    print("Notify via email")
 
 
 with DAG(
-    "my_dag",
-    schedule_interval="@daily",
-    start_date=datetime.datetime(2022, 1, 1),
-    catchup=False,
-    tags=["ETL-conntector", "version_1.0.0"],
-    description="ETL pipeline for connector",
-    dagrun_timeout=datetime.timedelta(hours=1),
-    on_failure_callback=send_failure_message,
+        "my_dag",
+        schedule_interval="@daily",
+        start_date=datetime.datetime(2022, 1, 1),
+        catchup=False,
+        tags=["ETL-pipeline", "version_1.0.0"],
+        description="ETL pipeline",
+        dagrun_timeout=datetime.timedelta(hours=1),
+        on_failure_callback=send_failure_message,
 ) as dag:
-    extract = PythonOperator(task_id="extract", python_callable=task_1)
+    download_invoices_op = PythonOperator(task_id="download_invoices", python_callable=download_invoices)
 
-    load = PythonOperator(task_id="load", python_callable=task_2)
+    transform_invoices_op = PythonOperator(task_id="transform_invoices", python_callable=transform_invoices)
 
-    transform_1 = PythonOperator(task_id="transform_1", python_callable=task_3_1)
+    save_to_database_op = PythonOperator(task_id="save_to_database", python_callable=save_to_database)
 
-    transform_2 = PythonOperator(task_id="transform_1", python_callable=task_3_2)
+    generate_report_op = PythonOperator(task_id="generate_report", python_callable=generate_report)
 
-    extract >> load >> [transform_1, transform_2]
+    notify_via_email_op = PythonOperator(task_id="notify_via_email", python_callable=notify_via_email)
+
+    ##TODO: Implement the workflow
+
